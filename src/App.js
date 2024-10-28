903e8a16953fdcecd4acc179940d526f1a7cf1c3
Ross Clelland <ross.clelland@upnup.ch>
2024-10-28 19:54:36 +0000

    Upnup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import { BrowserRouter as Router } from "react-router-dom"
+import AppContainer from "./AppContainer"
+
+const Scene = React.lazy(() => import("./vr/Scene"))
 
 function App() {
+  sessionStorage.setItem("room", "home")
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Router>
+        <React.Suspense fallback={null}>
+          <Scene />
+        </React.Suspense>
+        <AppContainer />
+      </Router>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import React from "react"
+import ReactDOM from "react-dom"
+import App from "./App"
+import "./styles/enterExperienceButton.scss"
+import "./styles/app.scss"
+import "./styles/layout.scss"
+import "./styles/home.scss"
+import "./styles/about.scss"
+import "./styles/projects.scss"
+import "./styles/contact.scss"
+import "./styles/fonts.scss"
+import "./styles/nav.scss"
+import "./styles/vr.scss"
+import "./styles/gradientTitle.scss"
+import "./styles/gradientButton.scss"
+import "./three/Experience"
+require("aframe")
+require("aframe-extras")
+require("aframe-super-keyboard")
+ReactDOM.render(<App />, document.getElementById("root"))
